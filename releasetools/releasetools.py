b875c7175616531bd5f34bef1bb4433cd9f50cf9
Nick Reuter <nreuter85@gmail.com>
2021-09-11 22:54:44 -0500

    odroidn2: update partition tables
    Change-Id: I9eee7516e959af160284367580c79aeffca2c73d

---
+++ releasetools/releasetools.py
@@ -0,0 +1,42 @@
+#!/bin/env python3
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import common
+import re
+
+def FullOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
+def IncrementalOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
+def AddImage(info, folder, basename, dest):
+  name = basename
+  data = info.input_zip.read(folder + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
+def AddDtbImage(info, folder, basename):
+  name = basename
+  data = info.input_zip.read(folder + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "/tmp/dtb.img");' % name);
+  info.script.AppendExtra('run_program("/system/bin/dd", "if=/tmp/dtb.img", "of=/dev/block/dtbs", "bs=1k", "count=256");');
+
+def PrintInfo(info, dest):
+  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+
+def OTA_InstallEnd(info):
+  if 'RADIO/dtb.img' in info.input_zip.namelist():
+    PrintInfo(info, "/dev/dtb")
+    AddDtbImage(info, "RADIO/", "dtb.img")
+  if 'RADIO/bootloader.img' in info.input_zip.namelist():
+    PrintInfo(info, "/dev/block/by-name/bootloader")
+    AddImage(info, "RADIO/", "bootloader.img", "/dev/block/by-name/bootloader")
+  return
